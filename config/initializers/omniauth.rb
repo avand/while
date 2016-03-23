9bdf834a655610e690869857c1414e9717260981
Avand Amiri <avand@avandamiri.com>
2016-03-22 18:15:19 -0700

    Signing in with Google prompts to select account

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider \
     :google_oauth2,
     Rails.application.secrets.google_client_id,
-    Rails.application.secrets.google_client_secret
+    Rails.application.secrets.google_client_secret,
+    prompt: :select_account
 end
