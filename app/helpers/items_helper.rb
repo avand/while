e0d2e2e5244bbd5c858c1a1bd8745ab8ff134f5c
Avand Amiri <avand@avandamiri.com>
2016-04-08 15:45:39 -0700

    Fixing a bug with progress bar width

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
 
   def progress_bar_width(number_of_items)
     return 0 if number_of_items.zero?
-    (Math.log10(number_of_items) * 40).round
+    (Math.log10(number_of_items + 0.5) * 40).round
   end
 
 end
