296c209cd38b205b23c630974b71746f7bc27df7
Avand Amiri <avand@avandamiri.com>
2016-10-23 23:45:34 -0700

    Removing background color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,6 @@ module ApplicationHelper
   def body_tag
     options = {}
 
-    if @root.present? && @root.color.present?
-      color = Color::RGB.by_hex @root.color
-      options[:style] = "background-color: ##{color.lighten_by(10).hex}"
-    end
-
     options[:class] = "#{controller_name.dasherize}-controller "
     options[:class] += "#{action_name.dasherize}-action"
 
