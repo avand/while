ed05026373875265fb18135fe477e3631bb351be
Avand Amiri <avand@avandamiri.com>
2016-11-11 21:19:52 -0800

    Only accessing for email from Google OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :google_oauth2,
     Rails.application.secrets.google_client_id,
     Rails.application.secrets.google_client_secret,
+    scope: "email",
     prompt: :select_account
 end
 
