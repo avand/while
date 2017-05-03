658fcf5795355fb53ca620ac4473411a2d668ac6
Avand Amiri <avand@avandamiri.com>
2017-05-02 23:41:33 -0700

    Fixing feedback submission for guests

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -9,7 +9,7 @@ class ApplicationMailer < ActionMailer::Base
     email = user.try(:email) || "guest@whilelist.com"
 
     mail({
-      from: "#{name} <#{user.try(:email) || ""}>",
+      from: "#{name} <#{email}>",
       to: "Avand Amiri <avand@avandamiri.com>",
       subject: "Feedback on While"
     })
